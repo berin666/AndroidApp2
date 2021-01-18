16bb7d645c310b774727e03bbf42f0dc5cc8efb8
berin <berinwins@gmail.com>
2021-01-18 13:47:02 +0530

    1234

--- app/src/main/java/com/demotxt/androidapp2/ActvityHome.java
+++ app/src/main/java/com/demotxt/androidapp2/ActvityHome.java
@@ -2,12 +2,28 @@ package com.demotxt.androidapp2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ActvityHome extends AppCompatActivity {
+    String myReceivedEmail;
+    String myReceivedName;
+    TextView txuseremail,tusername,Welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actvity_home);
+        myReceivedEmail=getIntent().getStringExtra("mykey");
+
+        myReceivedName=getIntent().getStringExtra("myuser");
+
+        txuseremail=findViewById(R.id.txuseremail);
+        txuseremail.setText(myReceivedEmail);
+
+        tusername=findViewById(R.id.tusername);
+        tusername.setText(myReceivedName);
+
+        Welcome=findViewById(R.id.Welcome);
+        Welcome.setText( "Welcome" + myReceivedName);
     }
 }
--- app/src/main/java/com/demotxt/androidapp2/MainActivity.java
+++ app/src/main/java/com/demotxt/androidapp2/MainActivity.java
@@ -8,10 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.time.Instant;
+
 public class MainActivity extends AppCompatActivity {
-    EditText etemail,etpass;
+    EditText etemail,etpass,etusername;
     Button btlogin;
-    String stringemail,stringpassword;
+    String stringemail,stringpassword,stringusername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +22,24 @@ public class MainActivity extends AppCompatActivity {
         btlogin=findViewById(R.id.btlogin);
         etemail=findViewById(R.id.etemail);
         etpass=findViewById(R.id.etpass);
+        etusername=findViewById(R.id.etusername);
 
         btlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 stringemail =etemail.getText().toString();
                 stringpassword =etpass.getText().toString();
+                stringusername=etusername.getText().toString();
 
                 if (stringemail.equals("abcd@gmail.com")&& stringpassword.equals("admin123")){
-                   startActivity(new Intent(MainActivity.this,ActvityHome.class));
+
+
+                   Intent mymovingIntent=new Intent(MainActivity.this,ActvityHome.class);
+
+                   mymovingIntent.putExtra("mykey",stringemail);
+                   mymovingIntent.putExtra("myuser",stringusername);
+
+                   startActivity(mymovingIntent);
 
                 }else {
                     Toast.makeText(MainActivity.this,"logonfail",Toast.LENGTH_LONG).show();
